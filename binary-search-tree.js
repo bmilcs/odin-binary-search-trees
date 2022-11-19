93ca30ff005f886aa1e18510adb5500a8b22ed9e
Bryan Miller <bmilcs@yahoo.com>
2022-11-19 17:50:37 -0500

    Swap let with const

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -25,8 +25,8 @@ class Tree {
 
   buildTree(array, start, end) {
     if (start > end) return null;
-    let mid = Math.ceil((start + end) / 2);
-    let root = new Node(array[mid]);
+    const mid = Math.ceil((start + end) / 2);
+    const root = new Node(array[mid]);
     root.left = this.buildTree(array, start, mid - 1);
     root.right = this.buildTree(array, mid + 1, end);
     return root;
