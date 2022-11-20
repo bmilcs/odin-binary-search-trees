85572430a345400ed8dbb28cd823da3dd9e291d0
Bryan Miller <bmilcs@yahoo.com>
2022-11-20 14:56:37 -0500

    Add insert, delete, find, level-order & dfs traversal methods

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -8,22 +8,15 @@ class Node {
 
 class Tree {
   constructor(array) {
-    // parse input
-    console.log("\nBalanced Binary Search Tree from an Array");
-    console.log(`- raw input:      ${array}`);
-    array = this.sortDedupeArray(array);
-    console.log(`- sorted/deduped: ${array}\n`);
-
-    // create balanced bst from array
-    this.root = this.buildTree(array, 0, array.length - 1);
-    this.print();
+    const parsedArray = this.sortDedupedArray(array);
+    this.root = this.buildTree(parsedArray);
   }
 
-  sortDedupeArray(array) {
+  sortDedupedArray(array) {
     return [...new Set(array)].sort((a, b) => (a > b ? +1 : -1));
   }
 
-  buildTree(array, start, end) {
+  buildTree(array, start = 0, end = array.length - 1) {
     if (start > end) return null;
     const mid = Math.ceil((start + end) / 2);
     const root = new Node(array[mid]);
@@ -32,76 +25,140 @@ class Tree {
     return root;
   }
 
-  print(node = this.root, prefix = " ", isLeft = true) {
-    if (node.right !== null)
-      this.print(node.right, `${prefix}${isLeft ? "│     " : "      "}`, false);
-    console.log(`${prefix}${isLeft ? "└───> " : "┌───> "}${node.data}`);
-    if (node.left !== null)
-      this.print(node.left, `${prefix}${isLeft ? "      " : "│     "}`, true);
-  }
-
   isLeaf(node) {
     if (node.left === null && node.right === null) return true;
     return false;
   }
 
   // insert value into bst
-  insert(value, node = this.root) {
-    if (!this.isLeaf(node)) {
-      node.left === null
-        ? this.insert(value, node.right)
-        : this.insert(value, node.left);
-      return;
+  // insert(value, node = this.root) {
+  //   // if node is NOT a leaf
+  //   if (!this.isLeaf(node)) {
+  //     // and node.left doesn't exist
+  //     node.left === null
+  //       ? this.insert(value, node.right) // right must contain a value, so traverse to it.
+  //       : this.insert(value, node.left); // else traverse to the left node
+  //     return;
+  //   }
+
+  //   // arrived a leaf: safe to add new node
+  //   value < node.value
+  //     ? (node.left = new Node(value))
+  //     : (node.right = new Node(value));
+  // }
+
+  insert(value, root = this.root) {
+    if (root === null) return new Node(value);
+    if (value > root.data) root.right = this.insert(value, root.right);
+    if (value < root.data) root.left = this.insert(value, root.left);
+    return root;
+  }
+
+  // delete value from bst
+  delete(value, root = this.root) {
+    if (root === null) return root;
+    // recurse through bst, comparing values
+    if (value < root.data) root.left = this.delete(value, root.left);
+    else if (value > root.data) root.right = this.delete(value, root.right);
+    else {
+      // value is NOT greater/less than current node (found it!)
+      if (root.left === null) return root.right;
+      else if (root.right === null) return root.left;
+
+      root.data = this.minValue(root.right);
+      root.right = this.delete(root.data, root.right);
+    }
+    return root;
+  }
+
+  // return smallest value in bst
+  minValue(root = this.root) {
+    let minVal = root.data;
+    while (root !== null) {
+      minVal = root.data;
+      root = root.left;
+    }
+    return minVal;
+  }
+
+  // finds node from a given value & returns node
+  find(value) {
+    let foundNode = null;
+    this.levelOrder((node) => {
+      if (node.data === value) foundNode = node;
+    });
+    return foundNode ? foundNode : null;
+  }
+
+  // traverse tree in breadth-first (level-order)
+  levelOrder(callback) {
+    const queue = [this.root];
+    const arrayOfValues = [];
+    while (queue.length > 0) {
+      const root = queue.shift();
+      // return array of values if NO function is provided
+      if (!callback) arrayOfValues.push(root.data);
+      // provide each node as argument to provided function
+      else callback(root);
+      if (root.left !== null) queue.push(root.left);
+      if (root.right !== null) queue.push(root.right);
+    }
+    if (!callback) return arrayOfValues;
+  }
+
+  // depth-first traversal: LDR
+  inorder(callback) {
+    const stack = [this.root];
+    let inorderArray = [];
+
+    while (stack.length) {
+      const current = stack.pop();
+      if (current.right) stack.push(current.right);
+      if (!callback) inorderArray.push(current.data);
+      else callback(current);
+      if (current.left) stack.push(current.left);
     }
 
-    value < node.value
-      ? (node.left = new Node(value))
-      : (node.right = new Node(value));
+    if (!callback) return inorderArray;
   }
 
-  delete(value) {
-    // delete value from bst
+  // depth-first traversal: DLR
+  preorder(callback) {
+    const stack = [this.root];
+    let inorderArray = [];
+
+    while (stack.length) {
+      const current = stack.pop();
+      if (!callback) inorderArray.push(current.data);
+      else callback(current);
+      if (current.left) stack.push(current.left);
+      if (current.right) stack.push(current.right);
+    }
+
+    if (!callback) return inorderArray;
   }
 
-  find(value) {
-    // finds node from a given value & returns node
+  // depth-first traversal: LRD
+  postorder(callback) {
+    const stack = [this.root];
+    let inorderArray = [];
+
+    while (stack.length) {
+      const current = stack.pop();
+      if (current.right) stack.push(current.right);
+      if (current.left) stack.push(current.left);
+      if (!callback) inorderArray.push(current.data);
+      else callback(current);
+    }
+
+    if (!callback) return inorderArray;
   }
 
-  levelOrder(aFunction) {
-    // accepts another function as parameter
-    // - traverse tree in breadth-first (level-order)
-    // - provide each node as argument to provided function
-    // - create iterative and recursive versions
-    // return array of values if NO function is provided
-    // * use array acting as a QUEUE to keep track of child nodes
-    //   that you have yet to traverse & add new ones to list
-  }
-
-  inorder(aFunction) {
-    // accepts function as parameter
-    // - traverse tree in depth-first order
-    // - yield each node to provided function given as argument
-    // return array of values if no function given
-  }
-
-  preorder(aFunction) {
-    // accepts function as parameter
-    // - traverse tree in depth-first order
-    // - yield each node to provided function given as argument
-    // return array of values if no function given
-  }
-
-  postorder(aFunction) {
-    // accepts function as parameter
-    // - traverse tree in depth-first order
-    // - yield each node to provided function given as argument
-    // return array of values if no function given
-  }
-
-  height(node) {
-    // accept node as parameter
-    // return its height
-    //        # of edges in the longest path FROM a node TO a leaf node
+  height(root = this.root) {
+    if (root === null) return 0;
+    const leftHeight = this.height(root.left);
+    const rightHeight = this.height(root.right);
+    return Math.max(leftHeight, rightHeight) + 1;
   }
 
   depth(node) {
@@ -119,6 +176,14 @@ class Tree {
     // rebalance an unbalanced tree
     // - traversal method to provide a new array to the buildTree function
   }
+
+  print(node = this.root, prefix = " ", isLeft = true) {
+    if (node.right !== null)
+      this.print(node.right, `${prefix}${isLeft ? "│     " : "      "}`, false);
+    console.log(`${prefix}${isLeft ? "└───> " : "┌───> "}${node.data}`);
+    if (node.left !== null)
+      this.print(node.left, `${prefix}${isLeft ? "      " : "│     "}`, true);
+  }
 }
 
 //
@@ -126,6 +191,20 @@ class Tree {
 //
 
 const bst = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-bst.insert(50);
+
+// traversal
+console.log(`level order:\t${bst.levelOrder()}`);
+console.log(`dfs inorder:\t${bst.inorder()}`);
+console.log(`dfs preorder:\t${bst.preorder()}`);
+console.log(`dfs postorder:\t${bst.postorder()}`);
+
+console.log(`min value: ${bst.minValue()}`);
+
 bst.insert(5);
+bst.insert(51);
 bst.print();
+
+bst.delete(4);
+bst.print();
+
+console.log(`height: ${bst.height()}`);
