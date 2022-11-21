b2e57fda003dbfbf1d5940071d4579c04cf13e8e
Bryan Miller <bmilcs@yahoo.com>
2022-11-21 12:30:23 -0500

    Fix in, pre, post order dfs traversal

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -94,8 +94,7 @@ class Tree {
     while (stack.length) {
       const current = stack.pop();
       if (current.right) stack.push(current.right);
-      if (!callback) inorderArray.push(current.data);
-      else callback(current);
+      !callback ? inorderArray.push(current.data) : callback(current);
       if (current.left) stack.push(current.left);
     }
 
@@ -109,10 +108,9 @@ class Tree {
 
     while (stack.length) {
       const current = stack.pop();
-      if (!callback) inorderArray.push(current.data);
-      else callback(current);
-      if (current.left) stack.push(current.left);
+      !callback ? inorderArray.push(current.data) : callback(current);
       if (current.right) stack.push(current.right);
+      if (current.left) stack.push(current.left);
     }
 
     if (!callback) return inorderArray;
@@ -127,8 +125,7 @@ class Tree {
       const current = stack.pop();
       if (current.right) stack.push(current.right);
       if (current.left) stack.push(current.left);
-      if (!callback) inorderArray.push(current.data);
-      else callback(current);
+      !callback ? inorderArray.push(current.data) : callback(current);
     }
 
     if (!callback) return inorderArray;
